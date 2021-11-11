b606340d4aab263502a99d28c6339be195aeb61a
John Askew <john.askew@deptagency.com>
2021-11-11 00:39:09 -0500

    Ensure logout uses current origin (instead of default / localhost

--- source/src/components/layout/header/AppHeader.js
+++ source/src/components/layout/header/AppHeader.js
@@ -125,7 +125,7 @@ const ProfileMenu = (props) => {
                     <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                         <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">Your Profile</a>
                         <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-1">Settings</a>
-                        <button href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2" onClick={() => logout()}>Sign out</button>
+                        <button href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2" onClick={() => logout({ returnTo: window.location.origin })}>Sign out</button>
                     </div>
                 </ClickAwayListener>
 
